ae7f5ae7d53cc97cc477d7f45242009431937892
Bence Lovas <bansoul1@gmail.com>
2017-06-25 18:42:11 +0200

    Printing dried

--- printing.py
+++ printing.py
@@ -3,14 +3,14 @@ import reports
 
 
 def main():
-    print(reports.count_games("game_stat.txt"))
-    print(reports.decide("game_stat.txt", 2012))
-    print(reports.get_latest("game_stat.txt"))
-    print(reports.count_by_genre("game_stat.txt", "RPG"))
-    print(reports.get_line_number_by_title("game_stat.txt", "Diablo III"))
-    print(reports.sort_abc("game_stat.txt"))
-    print(reports.get_genres("game_stat.txt"))
-    print(reports.when_was_top_sold_fps("game_stat.txt"))
+    print(reports.count_games("game_stat.txt"),
+          reports.decide("game_stat.txt", 2012),
+          reports.get_latest("game_stat.txt"),
+          reports.count_by_genre("game_stat.txt", "RPG"),
+          reports.get_line_number_by_title("game_stat.txt", "Diablo III"),
+          reports.sort_abc("game_stat.txt"),
+          reports.get_genres("game_stat.txt"),
+          reports.when_was_top_sold_fps("game_stat.txt"), sep="\n")
 
 if __name__ == "__main__":
     main()
