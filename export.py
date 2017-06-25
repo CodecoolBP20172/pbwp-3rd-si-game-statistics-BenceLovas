b88c8faca122040489c5137b2f5febd02ecaeb5e
Bence Lovas <bansoul1@gmail.com>
2017-06-25 19:16:20 +0200

    Exports printed reports to a txt file

--- export.py
+++ export.py
@@ -1,2 +1,12 @@
+# export.py - exports reports (to export.txt by default)
+import printing
+import sys
 
-# Export functions
+
+def main(file_name="export.txt"):
+    with open(file_name, "w") as f:
+        sys.stdout = f
+        printing.main()
+
+if __name__ == "__main__":
+    main()
